e72b3009bd9f52d5816cdce01ed6c54a220e7fa9
zenghao <742582470@qq.com>
2019-08-15 20:10:25 +0800

    save userinfo in db

--- src/main/java/com/learn/majiang/controller/AuthorizeController.java
+++ src/main/java/com/learn/majiang/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.learn.majiang.controller;
 
 import com.learn.majiang.dto.AccessTokenDTO;
 import com.learn.majiang.dto.GithubUser;
+import com.learn.majiang.mapper.UserMapper;
+import com.learn.majiang.model.User;
 import com.learn.majiang.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -26,6 +29,9 @@ public class AuthorizeController {
 
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    UserMapper userMapper;
 
     /**
      * 接收git的code
@@ -43,11 +49,20 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String token = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(token);
-        if(user!=null){
+        GithubUser githubUser = githubProvider.getUser(token);
+        if(githubUser!=null){
             //登录成功
+            //写入数据库user
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccount_id(String.valueOf(githubUser.getId()));
+            user.setGmt_create(System.currentTimeMillis());
+            user.setGmt_modified(user.getGmt_create());
+            userMapper.insert(user);
+            //将user存入seesion
             HttpSession session = request.getSession();
-            session.setAttribute("user",user);
+            session.setAttribute("user",githubUser);
             //注意redireact后面跟的是请求url不是页面的名字
             return "redirect:/";
         }else{
---
+++ src/main/java/com/learn/majiang/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.learn.majiang.mapper;
+
+import com.learn.majiang.model.User;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface UserMapper {
+
+  @Options(useGeneratedKeys = true,keyProperty = "id")
+  @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{account_id},#{name},#{token},#{gmt_create},#{gmt_modified}) ")
+  void insert(User user);
+
+}
